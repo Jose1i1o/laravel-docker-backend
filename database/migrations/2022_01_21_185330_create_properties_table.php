22dfb04a121766d01482d815da24c3139d0a48f6
Jose1i1o <guadix88@gmail.com>
2022-01-21 22:25:34 +0100

    create property model and add properties table

--- database/migrations/2022_01_21_185330_create_properties_table.php
+++ database/migrations/2022_01_21_185330_create_properties_table.php
@@ -15,6 +15,27 @@ class CreatePropertiesTable extends Migration
     {
         Schema::create('properties', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->text('street');
+            $table->string('number');
+            $table->string('city');
+            $table->string('province');
+            $table->string('country');
+            $table->string('status');
+            $table->string('type');
+            $table->string('contact_email');
+            $table->string('contact_phone');
+            $table->string('condition');
+            $table->string('room');
+            $table->string('bath');
+            $table->string('size');
+            $table->string('price');
+            $table->string('pet');
+            $table->string('garden');
+            $table->string('air_conditioning');
+            $table->string('swimming_pool');
+            $table->string('terrace');
             $table->timestamps();
         });
     }
