aa8ab52860fafcc97cb729eef2c026cd5383b1ae
Jose1i1o <guadix88@gmail.com>
2022-01-22 01:50:30 +0100

    server running and petitions completed on postman

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\BlogController;
+use App\Http\Controllers\API\PropertyController;
+use App\Http\Controllers\API\FavoriteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,5 +18,7 @@ Route::post('login', [AuthController::class, 'signin']);
 Route::post('register', [AuthController::class, 'signup']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::resource('blogs', BlogController::class);
+    Route::resource('properties', PropertyController::class);
+    Route::resource('favorites', FavoriteController::class);
+    // Route::resource('blogs', BlogController::class);
 });
